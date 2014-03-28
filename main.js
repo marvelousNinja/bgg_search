0a4276998e36fee7823239a0160264bc29bf0334
Alexander Sologub <alexsologoub@gmail.com>
2014-03-29 01:02:46 +0300

    Notifications API: Sample notification on extension start.

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
-alert("Main script loaded!");
+var notification = webkitNotifications.createNotification(
+  '48.png',  // icon url - can be relative
+  'Hello!',  // notification title
+  'Lorem ipsum...'  // notification body text
+);
+
+notification.show();

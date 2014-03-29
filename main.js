6764c227fdc67c5591080cac763b94370a2038dc
Alexander Sologub <alexsologoub@gmail.com>
2014-03-29 15:42:52 +0300

    Sorting and pooling: Results are getting sorted by BGG Rank and user ratings

--- main.js
+++ main.js
@@ -3,13 +3,57 @@ chrome.runtime.onMessage.addListener(function(request) {
   $.get("http://www.boardgamegeek.com/xmlapi/search",
     { "search": searchTerm },
     function(response) {
-      nodeList = response.querySelectorAll('boardgame')
+      nodeList = response.querySelectorAll('boardgame');
 
       ids = [];
 
-      for(var i = Math.min(nodeList.length, 5); i--; ids.unshift(nodeList[i].getAttribute('objectid')));
+      for(var i = nodeList.length; i--; ids.unshift(nodeList[i].getAttribute('objectid')));
 
-      console.log(ids);
+      $.get("http://www.boardgamegeek.com/xmlapi/boardgame/" + ids.join(),
+        { "stats": 1 },
+        function(response) {
+          nodeList = response.querySelectorAll('boardgame');
+
+          boardgames = [];
+
+          for(var i = nodeList.length; i--; i <= 0) {
+            node = nodeList[i];
+
+            boardgames.unshift({
+              "name": node.querySelector('name[primary]').innerHTML,
+              "geekRating": node.querySelector('bayesaverage').innerHTML,
+              "userRating": node.querySelector('average').innerHTML,
+              "gameRank": node.querySelector('rank[type=subtype]').getAttribute('value')
+            });
+          }
+
+          intCompare = function(a, b) {
+            return a - b;
+          }
+
+          compare = function(a, b) {
+            aRank = parseInt(a.gameRank);
+            bRank = parseInt(b.gameRank);
+
+            if (isNaN(aRank) && (isNaN(bRank))){
+              return intCompare(parseInt(b.userRating), parseInt(a.userRating));
+            }
+
+            if (isNaN(aRank)) {
+              return 1;
+            }
+
+            if (isNaN(bRank)) {
+              return -1;
+            }
+
+            return intCompare(aRank, bRank);
+          }
+
+          window.games = boardgames.sort(compare).slice(0, 5);
+          console.log(games);
+        }
+      );
     }
   );
 

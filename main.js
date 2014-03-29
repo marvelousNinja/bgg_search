c4d79bc872b87471a04defd4285203de9e594c46
Alexander Sologub <alexsologoub@gmail.com>
2014-03-29 19:03:33 +0300

    Basic version completed: Geek ratings are rendered

--- main.js
+++ main.js
@@ -1,5 +1,18 @@
+idCounter = 0;
 chrome.runtime.onMessage.addListener(function(request) {
   searchTerm = request.bgg.searchTerm;
+
+  var opt = {
+    type: "basic",
+    title: "BGG Search",
+    message: "Searching for: " + searchTerm + "...",
+    iconUrl: "icon.png"
+  }
+
+  notificationId = idCounter++;
+
+  chrome.notifications.create('' + notificationId, opt, function() {});
+
   $.get("http://www.boardgamegeek.com/xmlapi/search",
     { "search": searchTerm },
     function(response) {
@@ -52,25 +65,17 @@ chrome.runtime.onMessage.addListener(function(request) {
 
           boardgames = boardgames.sort(compare).slice(0, 5);
 
-          window.games = boardgames;
+          search_resuts = boardgames.map(function(boardgame) {
+            return {
+              title: boardgame.name,
+              message: 'Geek Rating: ' + boardgame.geekRating
+            }
+          });
+          
+          opt.type = "list";
+          opt.items = search_resuts;
 
-          // webkitNotifications.createNotification(
-          //   'icon.png',  // icon url - can be relative
-          //   'Games found:',  // notification title
-          //   "some multi\n line text" // notification body text
-          //   );//.show();
-
-          var opt = {
-            type: "progress",
-            title: "Primary Title",
-            message: "Primary message to display",
-            iconUrl: "icon.png",
-            items: [{ title: "Item1", message: "This is item 1."},
-                    { title: "Item2", message: "This is item 2."},
-                    { title: "Item3", message: "This is item 3."}]
-          }
-
-          chrome.notifications.create('', opt, function() {});
+          chrome.notifications.update('' + notificationId, opt, function() {});
         }
       );
     }
